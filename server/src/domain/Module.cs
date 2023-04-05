5af0f6524ff23662145c2a8f5a9abc2893a7e5fa
Andrey <sivyandrey2021@gmail.com>
2023-04-06 00:37:42 +0300

    Added module class

---
+++ server/src/domain/Module.cs
@@ -0,0 +1,23 @@
+namespace Domain
+{
+    public class Module
+    {
+        public Module(string name, string description, string img, string author)
+        {
+            this.name = name;
+            this.description = description;
+            this.img = img;
+            this.author = author;
+        }
+        public string Name{get => name; set => name = value;}
+        public string Description{get => description; set => description = value;}
+        public string Img{get => img; set => description = value;}
+        public string Author{get => author; set => author = value;}
+
+        private string name;
+        private string description;
+        private string img;
+        private string author;
+
+    }
+}
